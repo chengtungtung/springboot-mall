210ecb872880f9fdc08407e45f8213f782142a80
chengtungtung <hsintung.cheng@gmail.com>
2023-12-21 15:34:49 +0800

    初始化 Spring Boot project

---
+++ springboot-mall/src/test/java/com/hahow/tung/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hahow.tung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
